c92f53505fd4c208d6e87e1a071bff546eec8c2e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2025-08-20 10:02:47 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -15,7 +15,8 @@
   idx.workspace.onStart = {
     novnc = ''
       # Be forgiving on rebuilds
-      [ ! -f /home/user/.cleanup_done ] && find /home/user -mindepth 1 -maxdepth 1 ! -name 'idx-ubuntu22-gui' ! -name '.*' -exec rm -rf {} + 
+      [ ! -f /home/user/.cleanup_done ] && rm -rf /home/user/.gradle && rm -rf /home/user/.emu && find /home/user -mindepth 1 -maxdepth 1 ! -name 'idx-ubuntu22-gui' ! -name '.*' -exec rm -rf {} + 
+      
       touch /home/user/.cleanup_done
 
 
